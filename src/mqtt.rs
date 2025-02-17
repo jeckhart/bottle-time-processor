c587ab63273ec66ecc1a76dc1273f3ceeb76c3d0
John Eckhart <jeckhart@hey.com>
2025-02-17 02:36:30 -0500

    feat(influxdb): Push power measurement messages from mqtt to influxdb

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,4 +1,7 @@
-use crate::{error::ResultExt, models::KasaPowerMessage, mqtt_client::MqttClientManager};
+use crate::{
+    error::ResultExt, influxdb::InfluxDBWriter, models::KasaPowerMessage,
+    mqtt_client::MqttClientManager,
+};
 use rumqttc::MqttOptions;
 use std::time::Duration;
 
@@ -9,16 +12,29 @@ pub async fn setup_mqtt(opts: &crate::command_line::Options) -> miette::Result<M
 
     let (mqtt_client_manager, eventloop) = MqttClientManager::new(mqttoptions)?;
 
+    let influxdb = InfluxDBWriter::new(
+        opts.influxdb_url.clone(),
+        opts.influxdb_org.clone(),
+        opts.influxdb_token.clone(),
+        opts.influxdb_bucket.clone(),
+    );
+
     mqtt_client_manager
-        .subscribe(opts.topic.clone(), None, |message| {
-            let power_message: KasaPowerMessage =
-                serde_json::from_str(message.as_str()).with_serde_json_context()?;
-            let readings = power_message.into_readings();
-            for reading in readings {
-                println!("Reading: {:?}", reading);
-            }
-            // Process each reading...
-            Ok(())
+        .subscribe(opts.topic.clone(), None, move |message| {
+            let influxdb = influxdb.clone();
+            Box::pin(async move {
+                let power_message: KasaPowerMessage =
+                    serde_json::from_str(message.as_str()).with_serde_json_context()?;
+                let readings = power_message.into_readings();
+                for reading in readings {
+                    tracing::debug!("Writing reading to InfluxDB: {:?}", reading);
+                    influxdb
+                        .write_power_reading(&reading)
+                        .await
+                        .expect("Failed to write power reading to InfluxDB");
+                }
+                Ok(())
+            })
         })
         .await?;
 
