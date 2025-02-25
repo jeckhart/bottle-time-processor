16217d02bb65badc0df6ac44f9817ef45c1cd030
github-action-benchmark <github@users.noreply.github.com>
2025-02-25 02:13:38 +0000

    add Rust Benchmark (cargo) benchmark result for 6ce4315f64297bd887f588032a49ab227f0a1370

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1740449618839,
+  "repoUrl": "https://github.com/jeckhart/bottle-time-processor",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jeckhart@hey.com",
+            "name": "John Eckhart",
+            "username": "jeckhart"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6ce4315f64297bd887f588032a49ab227f0a1370",
+          "message": "Merge pull request #26 from jeckhart/feat/18-watchdog-timer\n\nAdd watchdog timer (#18)",
+          "timestamp": "2025-02-24T21:08:45-05:00",
+          "tree_id": "88c01c6e6e54ad41f24e5772150bd90e531cd622",
+          "url": "https://github.com/jeckhart/bottle-time-processor/commit/6ce4315f64297bd887f588032a49ab227f0a1370"
+        },
+        "date": 1740449618394,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "add",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
